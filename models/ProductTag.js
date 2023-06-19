42fed55edc9f2fe4bc49d2b3f8153d303de91417
Jeffrey V <jeffreyvicente@hotmail.com>
2023-06-18 19:01:46 -0700

    Defined all columns in productTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      AutoIncement: true,
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'prodcut',
+        id: 'id',
+      },
+    },
+    tag_name: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        id: 'id',
+      },
+    },
   },
   {
     sequelize,
