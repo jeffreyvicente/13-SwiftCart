e7cfa250bd3ce25ef59f0b5f2edf70919fd0381d
Jeffrey V <jeffreyvicente@hotmail.com>
2023-06-18 18:52:29 -0700

    Defined all columns in products.js

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      AutoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue:10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    catagory_id: {
+      type: DataTypes.INTERGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
