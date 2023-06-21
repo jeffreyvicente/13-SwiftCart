6c5c7ba5734fdee9e2fd92ddd6bfd4b9568b938a
Jeffrey V <jeffreyvicente@hotmail.com>
2023-06-21 14:25:55 -0700

    Added comment to the file

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
+// Loads environment variables from a .env file
 require('dotenv').config();
 
+// Importing the Sequelize library
 const Sequelize = require('sequelize');
 
+// Create a connection object
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
+  //Defines Database Name, Database User & Database Password
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      // Database location
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
@@ -12,4 +17,5 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
+// Exports the Sequelize instance for use in other modules
 module.exports = sequelize;
