d45d37af7c1682211bb958e827402a6988ce0dd6
Jeffrey V <jeffreyvicente@hotmail.com>
2023-06-21 15:06:50 -0700

    Added comment to the file

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+// Imports Express framework
 const express = require('express');
+// Imports routes module
 const routes = require('./routes');
-// import sequelize connection
+
+// Imports sequelize connection
 const sequelize = require('./config/connection');
 
+//Creating an instance of the Express
 const app = express();
+// Setting the port number for the server.
 const PORT = process.env.PORT || 3001;
 
+// Configures the Express application to handle requests with JSON 
 app.use(express.json());
+// Configuring the Express application to parse incoming requests with URL-encoded payloads
 app.use(express.urlencoded({ extended: true }));
 
+// Mounting the routes module on the application.
 app.use(routes);
 
 /*
@@ -17,7 +25,6 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
 */
-
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
