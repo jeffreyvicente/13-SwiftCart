ef83cfa6c5ea67cf46f9b947546fd7a86e4cdce5
Jeffrey V <jeffreyvicente@hotmail.com>
2023-06-21 14:32:17 -0700

    Added comment to the file

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,15 @@
+// Imports Sequelize
 const { Model, DataTypes } = require('sequelize');
 
+// Imports Sequelize instance from connection
 const sequelize = require('../config/connection.js');
 
+// Creates a Category model that extends Sequelize's Model class
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Defines the database columns
     id:{
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -19,12 +22,15 @@ Category.init(
     },
   },
   {
+    // Assigning the Sequelize instance to the 'sequelize' property
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
+    // Setting the model name to 'category'
     modelName: 'category',
   }
 );
 
+// Exporting the Category model
 module.exports = Category;
--- models/index.js
+++ models/index.js
@@ -1,32 +1,45 @@
-// import models
+// Imports the Product model
 const Product = require('./Product');
+
+// Imports the Category model
 const Category = require('./Category');
+
+// Imports the Tag model
 const Tag = require('./Tag');
+
+// Imports the ProductTag model
 const ProductTag = require('./ProductTag');
+
 
 // Products belongsTo Category
 Product.belongsTo(Category,{
+  // Defines the foreign key column in the Product model
   foreignKey:'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
+  // Defines the foreign key column in the Product model
   foreignKey:'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  // Specifies the intermediate model
   through: ProductTag,
+  // Defines the foreign key column in the model
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  // Specifies the intermediate model
   through: ProductTag,
+  // Defines the foreign key column in the model
   foreignKey: 'tag_id',
 });
 
-
+// Exporting the models
 module.exports = {
   Product,
   Category,
